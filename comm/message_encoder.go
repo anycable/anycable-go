3b27ac36a135a9b8689c38e7caa01589d64d56ac
Niklas Bichinger <niklas@bichinger.de>
2020-08-03 12:52:35 +0200

    cleanup

--- comm/message_encoder.go
+++ comm/message_encoder.go
@@ -5,19 +5,14 @@ import (
 )
 
 type MessageEncoder interface {
+	Unmarshal(data []byte, v interface{}) error
 	MarshalReply(message *common.Reply) ([]byte, error)
 	MarshalPing(message *common.PingMessage) ([]byte, error)
 	MarshalDisconnect(message *common.DisconnectMessage) ([]byte, error)
 	MarshalTransmissions(transmissions []string, message *common.Message) ([][]byte, error)
 	MarshalAuthenticateTransmissions(transmissions []string) ([][]byte, error)
-	Unmarshal(data []byte, v interface{}) error
 }
 
-var messageEncoder MessageEncoder
-
 func GetMessageEncoder() MessageEncoder {
-	if messageEncoder == nil {
-		messageEncoder = jsonEncoder{}
-	}
-	return messageEncoder
+	return jsonEncoder{}
 }
